9d08dd55eaa6696e5a999cead8b3aa2678c8d6af
fs <fs@imagine-orange.com>
2018-04-09 22:02:48 +0200

    define template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { PeopleService } from './people/people.service';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {PeopleService} from './people/people.service';
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <h1>Angular tabs</h1>
-    <ngx-tabs>
-      <ngx-tab tabTitle="People List">
-        <app-people-list [people]="people"></app-people-list>
-      </ngx-tab>
-      <ngx-tab tabTitle="Tab 2">Tab 2 Content</ngx-tab>
-    </ngx-tabs>
+    selector: 'app-root',
+    template: `
+        <h1>Angular tabs</h1>
+        <ngx-tabs>
+            <ngx-tab tabTitle="People List">
+                <app-people-list [people]="people"></app-people-list>
+            </ngx-tab>
+            <ngx-tab tabTitle="Tab 2">Tab 2 Content</ngx-tab>
+        </ngx-tabs>
 
-  `
+        <ng-template #personEdit>
+          This is the content of the template
+        </ng-template>
+
+    `
 })
-export class AppComponent implements OnInit {
-  people;
+export class AppComponent implements OnInit, AfterViewInit {
+    @ViewChild('personEdit') personEditTemplate;
+    people;
 
-  constructor(private peopleService: PeopleService) {}
+    constructor(private peopleService: PeopleService) {
+    }
 
-  ngOnInit() {
-    this.peopleService.getPeople().subscribe(data => {
-      this.people = data;
-    });
-  }
+    ngOnInit() {
+        this.peopleService.getPeople().subscribe(data => {
+            this.people = data;
+        });
+    }
+    ngAfterViewInit(){
+        console.log(this.personEditTemplate);
+    }
 }
